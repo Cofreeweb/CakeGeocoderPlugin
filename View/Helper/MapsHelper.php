384e32947efca39469b6bbb227ce9c0396b5f3c5
Alfonso Etxeberria <alfonso@Arambi.local>
2013-12-11 20:45:41 +0400

    Puesto sistema de iconos para el Helper

--- View/Helper/MapsHelper.php
+++ View/Helper/MapsHelper.php
@@ -18,6 +18,8 @@ class MapsHelper extends AppHelper
   
   public $language = null;
   
+  private $__cache = array(); 
+  
   public function beforeRender() 
   {
     if( !$this->request->is( 'ajax'))
@@ -29,6 +31,69 @@ class MapsHelper extends AppHelper
     }
   }
   
+/**
+ * Cache the result of the callback into the class.
+ *
+ * @param string|array $key
+ * @param callable $callback
+ * @return mixed
+ */
+	private function cache($key, Closure $callback) {
+		if (is_array($key)) {
+			$key = implode('-', $key);
+		}
+
+		if( isset( $this->__cache[$key])) {
+			return $this->__cache[$key];
+		}
+
+		$this->__cache[$key] = $callback();
+
+		return $this->__cache[$key];
+	}
+	
+	function jsonize( $foo)
+  {
+    $data = $this->jsonize_values( $foo);
+    extract( $data);
+
+    $json = json_encode( $foo);
+
+    foreach( $value_arr as $value)
+    {
+      $json = str_replace( '"' . $value .'"', $value, $json);
+    }
+
+
+    return trim( $json);
+  }
+	
+	function jsonize_values( $array)
+  {
+    $replace_keys = $value_arr = array();
+    
+    foreach( $array as $key => $value){
+      // Look for values starting with 'function('
+      if( !is_array( $value) && (strpos($value, 'function(') !== false || strpos($value, 'new') !== false)){
+        // Store function string.
+        $value_arr [] = $value;
+        // Replace function string in $foo with a 'unique' special key.
+        $value = '%' . $key . '%';
+        // Later on, we'll look for the value, and replace it.
+        $replace_keys [] = '"' . $value . '"';
+
+      }
+      elseif( is_array( $value))
+      {
+        $data = $this->jsonize_values( $value);
+        $replace_keys = array_merge( $replace_keys, $data ['replace_keys']);
+        $value_arr = array_merge( $value_arr, $data ['value_arr']);
+      }
+    }
+
+    return compact( 'replace_keys', 'value_arr');
+  }
+	
   public function create( $options = array())
   {
     $_options = array(
@@ -45,6 +110,31 @@ class MapsHelper extends AppHelper
     $this->markers = array();
   }
   
+/**
+ * Añade un marker al mapa que será renderizado
+ *
+ * Ejemplo de options
+ * 'lat' => 31.00000,
+ * 'lng' => 31.0000,
+ * 'title' => 'Un título',
+ * 'element' => '#map-layer2', // Es el elemento de donde tomará la información para mostrar en la ventana
+ * 'makerObject' => 'marker_2', // El nombre de objeto js del markder
+ * 'icon' => array(
+ *     'url' => WWW_ROOT .'img/marker.svg', // Si es tipo SVG, hay que dar la ruta absoluta del servidor, si es PNG, la ruta con el nombre del dominio
+ *     'type' => 'svg',
+ *     'options' => array(
+ *         'fillColor' => '#990000',
+ *         'fillOpacity' => 0.8,
+ *         'strokeColor' => 'gold',
+ *         'scale' => 0.5,
+ *         'strokeWeight' => 2
+  *    )
+ * )
+ *
+ * @param array $options 
+ * @return void
+ * @link https://developers.google.com/maps/documentation/javascript/reference?hl=es#Symbol (las opciones del icono)
+ */
   public function addMarker( $options)
   {
     $this->markers [] = $options;
@@ -69,15 +159,30 @@ EOF;
 
     foreach( $this->markers as $key => $marker)
     {
+      if( isset( $marker ['icon']['type']) && $marker ['icon']['type'] == 'svg')
+      {
+        $path = $this->cache( 'marker_'. $marker ['icon']['url'], function(){
+          App::uses( 'Util', 'Xml');
+          $xml = Xml::build( WWW_ROOT .'img/marker.svg');
+          $svg = Xml::toArray( $xml);
+          $path = $svg ['svg']['path']['@d'];
+          return $path;
+        });
+        
+        $marker ['icon']['options']['path'] = $path;
+        $icon_options = $this->jsonize( $marker ['icon']['options']);
+      }
+      
       $mapjs .= <<<EOF
       var latLng = new google.maps.LatLng( {$marker ['lat']}, {$marker ['lng']});
       var {$marker ['makerObject']} = new google.maps.Marker({
         position: latLng,
         title: '{$marker ['title']}',
-        map: {$this->map ['objectName']}.map
+        map: {$this->map ['objectName']}.map,
+        icon: $icon_options
       });
 EOF;
-      
+
       if( isset( $marker ['content']))
       {
         $mapjs .= <<<EOF
